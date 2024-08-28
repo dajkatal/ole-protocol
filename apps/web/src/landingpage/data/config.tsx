f3a2c7a205fc59e4e24f1e5b022ab544ab84be9a
dajkatal <dajkatal@gmail.com>
2024-08-28 19:33:39 +0300

    Changed config to remove sign up page.

--- apps/web/src/landingpage/data/config.tsx
+++ apps/web/src/landingpage/data/config.tsx
@@ -57,35 +57,7 @@ const siteConfig = {
         label: <FaGithub size="14" />,
       },
     ],
-  },
-  signup: {
-    title: 'Start building with Saas UI',
-    features: [
-      {
-        icon: FiCheck,
-        title: 'Accessible',
-        description: 'All components strictly follow WAI-ARIA standards.',
-      },
-      {
-        icon: FiCheck,
-        title: 'Themable',
-        description:
-          'Fully customize all components to your brand with theme support and style props.',
-      },
-      {
-        icon: FiCheck,
-        title: 'Composable',
-        description:
-          'Compose components to fit your needs and mix them together to create new ones.',
-      },
-      {
-        icon: FiCheck,
-        title: 'Productive',
-        description:
-          'Designed to reduce boilerplate and fully typed, build your product at speed.',
-      },
-    ],
-  },
+  }
 }
 
 export default siteConfig
